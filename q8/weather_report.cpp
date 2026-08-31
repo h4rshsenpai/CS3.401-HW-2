ae73f5e684501658a7a9405e22503fd8ba005ac9
h4rshsenpai <harshdeep.singh@research.iiit.ac.in>
2026-08-31 13:29:45 +0530

    add global reduction for scalars

--- q8/weather_report.cpp
+++ q8/weather_report.cpp
@@ -13,7 +13,7 @@ using namespace std;
 struct Measurement {
     ll timestamp;
     ll stationId;
-    double temperature;
+    double temp;
     double humidity;
     double pressure;
     double rainfall;
@@ -37,26 +37,34 @@ struct WeatherTotal {
     double pressureMin = 1000;
     double humidMin = 1000;
 
-    ll extreme_count = 0;
+    ll extreme_temp_count = 0;
+    ll count = 0;
+};
+
+struct stationStats {
+    ll count = 0;
+    double tempSum = 0.0;
+    double rainSum = 0.0;
 };
 
 void update_scalars(Measurement* M, WeatherTotal *W) {
         
-    if (M->temperature > 40.0 || M->temperature <= 0.0) 
-        W->extreme_count++;
+    W->count++;
+    if (M->temp > 40.0 || M->temp <= 0.0) 
+        W->extreme_temp_count++;
 
-    W->tempSum += M->temperature;
+    W->tempSum += M->temp;
     W->humidSum += M->humidity;
     W->pressureSum += M->pressure;
     W->rainfallSum += M->rainfall;
     W->windSum += M->windSpeed;
 
-    W->tempMax = (W->tempMax < M->temperature)? M->temperature : W->tempMax;
+    W->tempMax = (W->tempMax < M->temp)? M->temp : W->tempMax;
     W->humidMax = (W->humidMax < M->humidity)? M->humidity : W->humidMax;
     W->pressureMax = (W->pressureMax < M->pressure)? M->pressure : W->pressureMax;
     W->rainfallMax = (W->rainfallMax < M->rainfall)? M->rainfall : 
 
-    W->tempMin = (W->tempMin > M->temperature)? M->temperature : W->tempMin;
+    W->tempMin = (W->tempMin > M->temp)? M->temp : W->tempMin;
     W->humidMin = (W->humidMin > M->humidity)? M->humidity : W->humidMin;
     W->pressureMin = (W->pressureMin > M->pressure)? M->pressure : W->pressureMin;
 
@@ -74,8 +82,8 @@ int main(int argc, char** argv) {
 
 
     /*
-     ----------------------- Part 1 - READ -----------------------------
-       v1 -  Rank 0 reads the whole input file. Distributes to nodes. 
+     ----------------------- Part 1: READ -----------------------------
+       v1 -  Rank 0 reads the whole input file. 
        v2 - Each rank reads their own separate chunk into RAM - use mmap
     */
 
@@ -94,7 +102,7 @@ int main(int argc, char** argv) {
         for (int i = 0; i < N; ++i) {
             input >> data[i].timestamp
                   >> data[i].stationId
-                  >> data[i].temperature
+                  >> data[i].temp
                   >> data[i].humidity
                   >> data[i].pressure
                   >> data[i].rainfall
@@ -139,7 +147,7 @@ int main(int argc, char** argv) {
     MPI_Aint offsets[] = {
         offsetof(Measurement, timestamp),
         offsetof(Measurement, stationId),
-        offsetof(Measurement, temperature)
+        offsetof(Measurement, temp)
     };
 
     MPI_Type_create_struct(3, blockLengths, offsets, fieldTypes, &rawMeasurementType);
@@ -161,10 +169,8 @@ int main(int argc, char** argv) {
         MPI_COMM_WORLD
     );
 
-    MPI_Type_free(&measurementType);
 
-
-    /* ----------- LOCAL COMPUTATIONS -----------------
+    /* ----------- PART 2: LOCAL COMPUTATIONS -----------------
         v1 - compute scalars locally and create local maps for busiest interval and top K
             --> reduce scalars and merge maps at rank 0 and compute final results
 
@@ -173,30 +179,105 @@ int main(int argc, char** argv) {
                 hashed stationIDs
     */
 
-    // Each rank now has localData and can calculate its local statistics.
-
-    struct stationStats {
-        ll count = 0;
-        double tempSum = 0.0;
-        double rainSum = 0.0;
-    };
-    
     WeatherTotal W;
-    unordered_map<long long, stationStats> stats; // count, avg temp, tot rainfall
-    
+    Measurement localHottest, localColdest;
+
+    // local maps for top K and busiest interval
+    unordered_map<long long, stationStats> topK; // stationId -> {count, avg temp, tot rainfall}
+    unordered_map<double, long long> busiest;   // interval -> count
+
+    localHottest.temp = -1000; localColdest.temp = 1000;
+
     for (ll i = 0; i < localCount; i++) {
         
         Measurement *M = &localData[i];
         update_scalars(M, &W);
         
-        // update map for top K
-        auto& stat = stats[M->stationId]; 
-        stat->count++;
-        stat->tempSum += M->temperature;
-        stat.rainSum += M->rainfall;
-        
+        if (localHottest.temp < M->temp) localHottest.temp = M->temp;
+        if (localColdest.temp > M->temp) localColdest.temp = M->temp;
+    
+        // update top K
+        auto& entry = topK[M->stationId]; 
+        entry.count++;
+        entry.tempSum += M->temp;
+        entry.rainSum += M->rainfall;
+                   
+        // update busiest interval 
+        auto& interval = busiest[(double)M->timestamp/60];
+        entry.count++;
     }    
 
+    /* 
+        ---------- Part 3: Communication -----------
+        --> revamp entirely for v2
+    */
+    
+    // Reduce scalars at Rank 0
+
+    double localSums[5] = {W.tempSum, W.humidSum, W.pressureSum, W.rainfallSum, W.windSum};
+    double globalSums[5];
+
+    double localMaxes[5] = {W.tempMax, W.humidMax, W.pressureMax, W.rainfallMax, W.windMax};
+    double globalMaxes[5];
+
+    double localMins[3] = {W.tempMin, W.humidMin, W.pressureMin};
+    double globalMins[3];
+
+    MPI_Reduce(localSums, globalSums, 5, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(localMaxes, globalMaxes, 5, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(localMins, globalMins, 3, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+
+    Measurement hottestCandidate[P], coldestCandidate[P];     // each node sends 1 of each
+    MPI_Gather(&localHottest, 1, measurementType, hottestCandidate, P, measurementType, 0, MPI_COMM_WORLD);
+    MPI_Gather(&localColdest, 1, measurementType, coldestCandidate, P, measurementType, 0, MPI_COMM_WORLD);
+
+    MPI_Type_free(&measurementType);
+
+ 
+    // compute avg, min, max and total
+    double avgs[5];
+    for (int i = 0; i < 5; i++)
+        avgs[0] = globalSums[0] / N;
+
+
+    // select hottest and coldest candidate
+    Measurement globalHottest, globalColdest;
+    globalHottest.temp = -1000;
+    globalColdest.temp = 1000;
+
+    for (int i = 0; i < P; i++) {
+        // hottest first
+        if (globalHottest.temp == hottestCandidate[i].temp) {
+            if (globalHottest.timestamp < hottestCandidate[i].timestamp) {
+                globalHottest = hottestCandidate[i];
+            }
+            else { 
+                globalHottest = (globalHottest.stationId < hottestCandidate[i].stationId)? globalHottest : hottestCandidate[i];
+            }
+        }
+        else {
+            globalHottest = (globalHottest.temp < hottestCandidate[i].temp)? hottestCandidate[i] : globalHottest;
+        }
+        
+        //coldest
+        if (globalColdest.temp == coldestCandidate[i].temp) {
+            if (globalColdest.timestamp < coldestCandidate[i].timestamp) {
+                globalColdest = coldestCandidate[i];
+            }
+            else { 
+                globalColdest = (globalColdest.stationId < coldestCandidate[i].stationId)? globalColdest : coldestCandidate[i];
+            }
+        }
+        else {
+            globalColdest = (globalColdest.temp > coldestCandidate[i].temp)? hottestCandidate[i] : globalColdest;
+        }
+
+    }
+    // mins, maxs already calculated
+
+    
+    
     MPI_Finalize();
     return 0;
 }
